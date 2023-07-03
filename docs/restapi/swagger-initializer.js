9657ff38ac49cfbda45354c2a7ea7336c28f03f5
Dele Olajide <dele@4ng.net>
2023-07-03 09:51:18 +0100

    add restapi app

---
+++ docs/restapi/swagger-initializer.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: location.host.indexOf("comitfs.github.io") > -1 ? "https://localhost:7443/teams/api/swagger.json" : location.protocol + "//" + location.host + "/teams/api/swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    onComplete: function() {
+		const urlify = (text) => {
+			var urlRegex = /(https?:\/\/[^\s]+)/g;
+			return text.replace(urlRegex, '<a target="_blank" href="$1">$1</a>');
+		}
+
+		const urlParam = ()	=> {
+			let value = null;
+			let results = new RegExp('[\\?&]t=([^&#]*)').exec(window.location.href);
+			
+			if (results) {
+				value = unescape(results[1] || undefined);
+			}
+			
+			if (!value) {
+				value = sessionStorage.getItem("cas.workflow.config." + name);
+			}	
+
+			if (!value) {
+				value = prompt("Enter User Access Token");
+			}			
+				
+			if (value) {
+				sessionStorage.setItem("cas.workflow.config." + name, value);
+			}
+
+			console.debug("urlParam get token", value);				
+			return value;
+		}		
+		
+		let token = urlParam();
+		
+		if (token) {
+			window.ui.preauthorizeApiKey("authorization", token);		
+			const source = new EventSource( "/teams/web-sse?token=" + token);
+			
+			source.onerror = event => {
+				console.debug("EventSource - onError", event);				
+			};
+
+			source.addEventListener('onNotify', async event => {
+				const msg = JSON.parse(event.data);
+				document.getElementById("status").innerHTML = urlify(msg.text);
+				console.debug("EventSource - onMessage", msg);
+			});
+			
+			source.addEventListener('onConnect', async event => {
+				console.debug("EventSource - onConnect");		
+			});	
+		}			
+    },	
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
