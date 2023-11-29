62788599f175562a6d5952e59414748b7e7800c9
Dele Olajide <dele@4ng.net>
2023-11-29 20:20:29 +0000

    remove webauthn

--- docs/deploy/index.js
+++ docs/deploy/index.js
@@ -16,22 +16,17 @@ var cas_workflow_api = (function(api)
 		if (!username || !password) {
 			registerUser();
 			
-			WebAuthnGoJS.CreateContext(JSON.stringify({RPDisplayName: "CAS Workflow", RPID: window.location.hostname, RPOrigin: window.location.origin}), (err, val) => {
-				if (err) {
-					handleError(err);
+			navigator.credentials.get({password: true}).then(function(credential) {
+				console.debug("window.load credential", credential);	
+				
+				if (credential) {
+					setCredentials(credential.id, credential.password);
+				} else {
+					registerUser();							
 				}
-				navigator.credentials.get({password: true}).then(function(credential) {
-					console.debug("window.load credential", credential);	
-					
-					if (credential) {
-						loginUser(credential.id, credential.password);
-					} else {
-						registerUser();							
-					}
-				}).catch(function(err){
-					console.error("window.load credential error", err);	
-					registerUser();					
-				});	
+			}).catch(function(err){
+				console.error("window.load credential error", err);	
+				registerUser();					
 			});				
 		} else {
 			handleCredentials(username, password);
@@ -90,79 +85,7 @@ var cas_workflow_api = (function(api)
         link.type = 'text/css';
         link.href = name;
         head.appendChild(link);
-    }
-
-	function bufferDecode(value) {
-	  return Uint8Array.from(atob(value), c => c.charCodeAt(0));
-	}
-
-	function bufferEncode(value) {
-	  return btoa(String.fromCharCode.apply(null, new Uint8Array(value)))
-		.replace(/\+/g, "-")
-		.replace(/\//g, "_")
-		.replace(/=/g, "");;
-	}
-
-	function loginUser(username, userStr) {
-		const user = JSON.parse(userStr);
-		console.debug("loginUser", user);		
-		
-		const loginCredRequest = (credentialRequestOptions) => {
-			credentialRequestOptions.publicKey.challenge = bufferDecode(credentialRequestOptions.publicKey.challenge);
-			credentialRequestOptions.publicKey.allowCredentials.forEach(function (listItem) {
-			  listItem.id = bufferDecode(listItem.id)
-			});
-
-			return navigator.credentials.get({
-			  publicKey: credentialRequestOptions.publicKey
-			})
-		}
-
-		WebAuthnGoJS.BeginLogin(userStr, (err, data) => {
-			if (err) {
-				console.error("Login failed", err);				
-				handleError(err);
-			}
-
-			data = JSON.parse(data);
-			user.authenticationSessionData = data.authenticationSessionData;
-
-			loginCredRequest(data.credentialRequestOptions).then((assertion) => {
-				let authData = assertion.response.authenticatorData;
-				let clientDataJSON = assertion.response.clientDataJSON;
-				let rawId = assertion.rawId;
-				let sig = assertion.response.signature;
-				let userHandle = assertion.response.userHandle;
-
-				const finishLoginObj = {
-					id: assertion.id,
-					rawId: bufferEncode(rawId),
-					type: assertion.type,
-					response: {
-						authenticatorData: bufferEncode(authData),
-						clientDataJSON: bufferEncode(clientDataJSON),
-						signature: bufferEncode(sig),
-						userHandle: bufferEncode(userHandle)
-					}
-				}
-
-				const loginBodyStr = JSON.stringify(finishLoginObj);
-				const authSessDataStr = JSON.stringify(user.authenticationSessionData)
-
-				WebAuthnGoJS.FinishLogin(userStr, authSessDataStr, loginBodyStr, (err, result) => {
-					console.debug("Login result", username, err, result);
-					
-					if (err) {
-						handleError(err);
-					}						
-					setCredentials(username, userStr);
-				});
-			}).catch((err) => {
-				console.error("Login failed", err);
-				handleError(err);				
-			});
-	  });
-	}	
+    }	
 
 	function urlParam(name)	{
 		let value = null;
@@ -191,7 +114,7 @@ var cas_workflow_api = (function(api)
 		let url = urlParam("u");
 				
 		if (username && token && url) {	
-			createUserCredentials(username, token);
+			registerCredential(username, token);
 			
 		} else {
 			const example = document.querySelector(".docs-DialogExample-close");
@@ -233,99 +156,8 @@ var cas_workflow_api = (function(api)
 			localStorage.setItem("cas.workflow.config.i", username);
 			
 			console.debug("actionHandler", url, token, username);
-			createUserCredentials(username, token);
+			registerCredential(username, token);
 		}
-	}
-	
-	function createUserCredentials(username, token) 
-	{
-		if (!username || username.trim() === "" || !token || token.trim() === "") {
-			handleError("bad username or token");
-		}
-		
-		const displayName = username;
-		username = username.toLocaleLowerCase().replaceAll(" ", "");		
-
-		const createPromiseFunc = (credentialCreationOptions) => 
-		{
-			credentialCreationOptions.publicKey.challenge = bufferDecode(credentialCreationOptions.publicKey.challenge);
-			credentialCreationOptions.publicKey.user.id = bufferDecode(credentialCreationOptions.publicKey.user.id);
-			
-			if (credentialCreationOptions.publicKey.excludeCredentials) 
-			{
-			  for (var i = 0; i < credentialCreationOptions.publicKey.excludeCredentials.length; i++) {
-				credentialCreationOptions.publicKey.excludeCredentials[i].id = bufferDecode(credentialCreationOptions.publicKey.excludeCredentials[i].id);
-			  }
-			}
-
-			return navigator.credentials.create({
-			  publicKey: credentialCreationOptions.publicKey
-			})
-		}
-
-		const user = {
-			id: Math.floor(Math.random() * 1000000000),
-			name: username,
-			displayName: displayName,
-			credentials: [],
-		};
-  
-		const userStr = JSON.stringify(user);
-
-		WebAuthnGoJS.BeginRegistration(userStr, (err, data) => 
-		{
-			if (err) {
-				console.error("Registration failed", err);				
-				handleError(err);
-			}
-			
-			data = JSON.parse(data);
-			user.registrationSessionData = data.registrationSessionData;
-
-			createPromiseFunc(data.credentialCreationOptions).then((credential) => {
-				let attestationObject = credential.response.attestationObject;
-				let clientDataJSON = credential.response.clientDataJSON;
-				let rawId = credential.rawId;
-
-				const registrationBody = {
-					id: credential.id,
-					rawId: bufferEncode(rawId),
-					type: credential.type,
-					response: {
-					  attestationObject: bufferEncode(attestationObject),
-					  clientDataJSON: bufferEncode(clientDataJSON),
-					},
-				};
-
-				// Stringify
-				const regBodyStr = JSON.stringify(registrationBody);
-				const sessDataStr = JSON.stringify(user.registrationSessionData)
-
-				WebAuthnGoJS.FinishRegistration(userStr, sessDataStr, regBodyStr, (err, result) => 
-				{
-					if (err) {
-						console.error("Registration failed", err);				
-						handleError(err);
-					}
-					
-					const credential = JSON.parse(result);
-					credential.github_token = token;
-					user.credentials.push(credential);						
-					registerCredential(username, JSON.stringify(user));
-
-				});
-				
-			}).catch((err) => {
-				console.error("Registration failed", err);
-				handleError(err);
-			});
-		})
-	}
-	
-	async function hashCode(target){
-	   var buffer = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(target));
-	   var chars = Array.prototype.map.call(new Uint8Array(buffer), ch => String.fromCharCode(ch)).join('');
-	   return btoa(chars);
 	}
 	
 	function registerCredential(id, pass) {
